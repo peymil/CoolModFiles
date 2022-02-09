8f19c2c513ec60ebea52f5242c07124a9b635d37
Emre Badem <emrebadem_@hotmail.com>
2022-02-09 23:45:21 +0300

    refactor: Put every logic to store

--- src/lib/stores/player.ts
+++ src/lib/stores/player.ts
@@ -1,32 +1,67 @@
-import type { ChiptuneJsPlayer, MetaData } from 'src/global';
 import { get, writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
 class PlayerStore {
-    playerInstance?: ChiptuneJsPlayer;
+    volume: Writable<number[]> = writable([0]);
+    playerInstance: ChiptuneJsPlayer;
     buffer: AudioBuffer;
     loading: Writable<boolean> = writable(true);
     isPlaying: Writable<boolean> = writable(false);
-    metaData: Writable<MetaData> = writable(null);
-
+    metaData: Writable<ReturnType<ChiptuneJsPlayer['metadata']>> = writable(null);
+    position: Writable<number[]> = writable([0]);
+    duration: Writable<number> = writable(0)
     setup() {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
-        this.playerInstance = new ChiptuneJsPlayer(new ChiptuneJsConfig(0, 'volume'));
+        this.playerInstance = new ChiptuneJsPlayer(new ChiptuneJsConfig(0, this.volume[0]));
         console.log('[player] player initialized');
+        
+    }
+
+    private initializeDurationWatch() {
+        console.log("this",this);
+        
+        let positionInterval
+        this.isPlaying.subscribe((isPlaying) => {
+            if (isPlaying || !positionInterval) {
+                positionInterval = setInterval(() => {
+                    const newPosition = this.playerInstance.getPosition();
+                    this.position.set([newPosition]);
+                    console.log("newPOsition",newPosition);
+                    
+                    if (newPosition === 0) {
+                        clearInterval(positionInterval);
+                    }
+                }, 300);
+            } else {
+                clearInterval(positionInterval);
+            }
+        });
     }
 
     async load(id: string) {
         this.buffer = await this.playerInstance.load(`jsplayer.php?moduleid=${id}`);
         this.loading.set(false);
-
         console.log('[player] buffer loaded, id:', id);
     }
 
+    seek(value: number) {
+        console.log("seek",this);
+        
+        // this.position.set([value])
+        return this.playerInstance.seek(value);
+    }
+
+    setVolume(volume:number){
+        this.volume.set([volume])
+        this.playerInstance.setVolume(volume)
+    }
     play() {
         this.playerInstance.play(this.buffer);
+        this.metaData.set(this.playerInstance.metadata());
+        this.duration.set(this.playerInstance.duration())
+        this.initializeDurationWatch()
         this.isPlaying.set(true);
-        this.metaData.set(this.getMetaData());
         console.log('[player] Player start');
     }
 
@@ -41,22 +76,11 @@ class PlayerStore {
         this.isPlaying.set(false);
         console.log('[player] Player stop');
     }
-
     getPosition() {
         return this.playerInstance.getPosition();
     }
 
-    seek(value: number) {
-        return this.playerInstance.seek(value);
-    }
 
-    duration() {
-        return this.playerInstance.duration();
-    }
-
-    getMetaData() {
-        return this.playerInstance.metadata();
-    }
 }
 
 export default new PlayerStore();
