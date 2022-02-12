194407a878a6f6cbbede5e5434df5dcea1a02419
Emre Badem <emrebadem_@hotmail.com>
2022-02-12 06:13:51 +0300

    feat: Add basic voice button.

---
+++ src/components/VoiceButton.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import Slider from 'svelte-range-slider-pips';
+    export let volume;
+    export let onVolumeChange;
+    let isVolumeButtonPressed = false;
+</script>
+
+<div class="relative">
+    <div on:click={() => isVolumeButtonPressed = !isVolumeButtonPressed}>
+        {$volume ? "Volume" : 'X'}
+    </div>
+    {#if isVolumeButtonPressed}
+        <div class="absolute bottom-full w-full flex justify-center z-10">
+            <div class="bg-black">
+            <Slider
+                vertical
+                range="min"
+                min={0}
+                max={100}
+                springValues={{ stiffness: 1, damping: 1 }}
+                bind:values={$volume}
+                on:change={({ detail: { value } }) => onVolumeChange(value)}
+            />
+        </div>
+        </div>
+    {/if}
+</div>
