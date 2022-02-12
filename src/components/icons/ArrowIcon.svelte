dffe52815412c258c91193bb6be8070c22974be9
Emre Badem <emrebadem_@hotmail.com>
2022-02-12 04:15:08 +0300

    feat: Try to add svgs.

---
+++ src/components/icons/ArrowIcon.svelte
@@ -0,0 +1,40 @@
+
+
+  
+    <script>
+                    export let width
+                export let height
+export let className
+export let style
+</script>
+<svg width="1em" height="1em" viewBox="0 0 52 18" fill="none" >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M25.26.416L0 15l1.5 2.598L25.74 3.603l24.24 13.995L51.48 15 26.222.416 25.981 0l-.24.139L25.5 0l-.24.416z"
+        fill="#BD00FF"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M25.26.416L0 15l1.5 2.598L25.74 3.603l24.24 13.995L51.48 15 26.222.416 25.981 0l-.24.139L25.5 0l-.24.416z"
+        fill="url(#arrow_svg__paint0_linear)"
+      />
+      <defs>
+        <linearGradient
+          id="arrow_svg__paint0_linear"
+          x1={49}
+          y1={15}
+          x2={4.5}
+          y2={13.5}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#00B8FF" />
+          <stop offset={1} stopColor="#fff" stopOpacity={0} />
+        </linearGradient>
+      </defs>
+    </svg>
+  
+
+
+
